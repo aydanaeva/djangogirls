2e77fee6f1d87395d0d714a14c0b140cc1215592
aydan <aydanaeva@gmail.com>
2025-03-16 21:47:54 +0400

    turn off debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9kf&8bw*u8=2i6q+a4mx-bn5^)9le!#3r0*wn24l35)-819m@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
